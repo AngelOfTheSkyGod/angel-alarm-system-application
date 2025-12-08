51710069875573f17f2035713d06d8a03d53f491
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2025-12-08 00:58:25 -0600

    f

--- application.py
+++ application.py
@@ -15,11 +15,9 @@ import os
 import requests
 import json
 # Config.set('graphics', 'fullscreen', 'auto')  # use 'auto' to match display resolution
-from flask_compress import Compress
 imageCount = 0
 # -- REST server side (Flask) --
 flask_app = Flask(__name__)
-Compress(flask_app)
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
